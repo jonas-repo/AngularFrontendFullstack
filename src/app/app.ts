e344e9fc3bf51a94ebf75ed4a8d37b72343cd4b4
JonasMaldo <isc.jonas.maldonado@gmail.com>
2025-07-08 15:29:07 -0600

    ngfor test for users in app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'Hola mundo Angular';
+  protected title = 'Hello world angular from component!';
+  users = ['Peter','Mary','Jhon','Andrei'];
 }
