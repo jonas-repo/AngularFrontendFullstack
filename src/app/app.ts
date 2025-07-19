f79234a94f37b8d025e5c477ec06eda2d6453093
JonasMaldo <isc.jonas.maldonado@gmail.com>
2025-07-19 09:11:39 -0600

    adding a new component counter with a dynamic counter using a button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CommonModule} from '@angular/common';
+import { Counter } from './counter/counter';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, Counter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
