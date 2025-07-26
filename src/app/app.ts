6b8bc3a2ce56a03db4bd86929979455016fa73b7
JonasMaldo <isc.jonas.maldonado@gmail.com>
2025-07-26 09:32:57 -0600

    adding shared attributes from child to parent component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CommonModule} from '@angular/common';
 import { Counter } from './counter/counter';
@@ -10,12 +10,23 @@ import { Counter } from './counter/counter';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+  
   protected title = 'Hello world angular from component!';
   subtitle = 'State session counter'
   users = ['Peter','Mary','Jhon','Andrei'];
   visible: boolean = false;
+  counter: number = 0;
+
+  ngOnInit(): void {
+    this.counter = 
+   parseInt(sessionStorage.getItem('counter')!) || 0;
+  } 
   setVisible(): void {
     this.visible = this.visible? false : true;
+  }
+
+  setCounter(counter: number):void{
+    this.counter = counter;
   }
 }
--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -11,6 +11,8 @@ counter: number = 0;
 
 @Input() title!:string;
 
+@Output() counterEmmit : EventEmitter<number> = new EventEmitter();
+
  ngOnInit(): void {
    this.counter = 
    parseInt(sessionStorage.getItem('counter')!) || 0;
@@ -19,5 +21,6 @@ counter: number = 0;
  setCounter(): void{
   this.counter = this.counter +1;
   sessionStorage.setItem('counter', this.counter.toString());
+  this.counterEmmit.emit(this.counter);
  }
 }
